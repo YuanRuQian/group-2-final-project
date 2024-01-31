aebdb76afb0c96a4319c17da1689fb27b8be3391
Tangerine233 <hekainuo@foxmail.com>
2024-01-31 02:06:55 -0500

    Extract Review functions to it's own ViewModel
    Making a reusable ReviewViewModel to be use in Destination Detail and personal Profile. Separated from ProfileViewModel's review functions.

--- app/src/main/java/group/two/tripplanningapp/viewModels/ProfileViewModel.kt
+++ app/src/main/java/group/two/tripplanningapp/viewModels/ProfileViewModel.kt
@@ -2,7 +2,6 @@ package group.two.tripplanningapp.viewModels
 
 import android.net.Uri
 import android.util.Log
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -10,10 +9,7 @@ import com.google.firebase.auth.userProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import androidx.compose.runtime.State
-import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
-import group.two.tripplanningapp.data.Review
-import group.two.tripplanningapp.utilities.ProfileReviewSortOptions
 
 
 class ProfileViewModel : ViewModel() {
@@ -31,13 +27,9 @@ class ProfileViewModel : ViewModel() {
     private val _profileImageUrl = mutableStateOf("")
     val profileImageUrl: State<String> get() = _profileImageUrl
 
-    private val _reviews: MutableState<List<Review>> = mutableStateOf(emptyList())
-    val reviews: MutableState<List<Review>> get() = _reviews
-
     init {
         getProfileImage()
         getDisplayName()
-        getUserReviews()
     }
 
     // User Display Name
@@ -130,125 +122,12 @@ class ProfileViewModel : ViewModel() {
         }
     }
 
-    // User Reviews
-    private fun getUserReviews() {
-        // Get Review IDs
-        val reviewsCollection = firestore.collection("userProfiles").document(user?.uid?:"")
-            .collection("reviews")
-
-        reviewsCollection.addSnapshotListener { snapshot, error ->
-            if (error != null) {
-                Log.e(TAG, "Error getting user review IDs: $error")
-            }
-
-            // Step 1: Get User Reviews' IDs as List<String>
-            val userReviewIds = snapshot?.documents?.mapNotNull { document ->
-                document.id
-            } ?: emptyList()
-
-            // Step 2: fetch reviews from the List of IDs
-            fetchReviews(userReviewIds)
-        }
-    }
-    private fun fetchReviews(reviewIds: List<String>) {
-        // Initialize an empty list to store fetched reviews
-        val fetchedReviews = mutableListOf<Review>()
-
-        // Loop through each review ID and fetch the corresponding review
-        reviewIds.forEach { reviewId ->
-            val reviewDocumentRef = firestore.collection("reviews").document(reviewId)
-
-            reviewDocumentRef.get().addOnSuccessListener { documentSnapshot ->
-                val review = documentSnapshot.toObject(Review::class.java)
-                review?.let {
-                    // Add the fetched review to the list
-                    it.reviewId =reviewId
-                    fetchedReviews.add(it)
-                }
-
-                // Check if all reviews have been fetched
-                if (fetchedReviews.size == reviewIds.size) {
-                    // Update with the fetched reviews
-                    _reviews.value = fetchedReviews
-                }
-
-            }.addOnFailureListener { exception ->
-                Log.e(TAG, "Error getting review with ID $reviewId: $exception")
-            }
-        }
-    }
-
-    // Update Reviews
-    fun updateReview(reviewId: String, newContent: String, showSnackbarMessage: (String) -> Unit) {
-        val reviewDocumentRef =
-            firestore.collection("reviews").document(reviewId)
-
-        val newTimestamp = Timestamp.now()
-        val updatedReview = hashMapOf(
-            "content" to newContent,
-            "timestamp" to newTimestamp
-        )
-
-        reviewDocumentRef.update(updatedReview.toMap())
-            .addOnSuccessListener {
-                showSnackbarMessage("Review updated successfully.")
-                // Assuming you want to refresh the reviews after an update
-                getUserReviews()
-            }
-            .addOnFailureListener { exception ->
-                Log.e(TAG, "Error updating review: $exception")
-                showSnackbarMessage("Error updating review.")
-            }
-    }
-
-    // Delete Reviews
-    fun deleteReview(reviewId: String, showSnackbarMessage: (String) -> Unit) {
-
-        // Step 1: Delete in reviews collection: reviews\{reviewID}
-        val reviewDocumentRef =
-            firestore.collection("reviews").document(reviewId)
-        reviewDocumentRef.delete()
-            .addOnSuccessListener {
-
-                // Step 2: Delete in userProfile: userProfile\{userID}\reviews\{reviewID}
-                val userReviewsCollectionRef =
-                    firestore.collection("userProfiles").document(user?.uid ?: "")
-                        .collection("reviews").document(reviewId)
-                userReviewsCollectionRef.delete()
-                    .addOnSuccessListener {
-
-                        // Post Deletions
-                        showSnackbarMessage("Review deleted successfully.")
-                        getUserReviews() // refresh the reviews
-                    }
-                    .addOnFailureListener { exception ->
-                        Log.e(TAG, "Error deleting review: $exception")
-                        showSnackbarMessage("Error deleting review.")
-                    }
-            }
-            .addOnFailureListener { exception ->
-                Log.e(TAG, "Error deleting review: $exception")
-                showSnackbarMessage("Error deleting review.")
-            }
-    }
-
-    // Function to get sorted reviews based on the selected sorting option
-    fun profileSortReviews(sortOption: ProfileReviewSortOptions) {
-        when (sortOption) {
-            ProfileReviewSortOptions.Date -> _reviews.value = reviews.value.sortedByDescending { it.timestamp }
-            ProfileReviewSortOptions.Location -> _reviews.value =reviews.value.sortedBy { it.destination }
-            ProfileReviewSortOptions.Rating -> _reviews.value =reviews.value.sortedByDescending { it.rating }
-        }
-    }
-
-
-
     // Delete Account
-    fun deleteAccount(showSnackbarMessage: (String) -> Unit, logout:()->Unit){
+    fun deleteAccount(reviewViewModel: ReviewViewModel, showSnackbarMessage: (String) -> Unit, logout:()->Unit){
 
         // Step 1: Delete all reviews by the user in database
-        _reviews.value.forEach{ eachReview ->
-            deleteReview(eachReview.reviewId, showSnackbarMessage = {Log.e(TAG, "Delete Review: $eachReview")})
+        reviewViewModel.yourReviews.value.forEach{ eachReview ->
+            reviewViewModel.deleteReview(eachReview.reviewId, showSnackbarMessage = {Log.e(TAG, "Delete Review: $eachReview")})
         }
 
         // Step 2: Delete user profile in data base
